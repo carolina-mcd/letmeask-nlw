3a04e3403d90451fe54c3725a34c0bebe0216bc9
carolina-mcd <carolina-macedo93@hotmail.com>
2021-06-24 21:42:33 -0300

    aula 4 finalizada

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { AuthContextProvider } from './contexts/AuthContext';
+
+import { AdminRoom } from './pages/AdminRoom';
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 import { Room } from './pages/Room';
+
+import { AuthContextProvider } from './contexts/AuthContext';
 
 export function App() {
   return (
@@ -12,6 +15,8 @@ export function App() {
           <Route path="/" exact component={Home} />
           <Route path="/rooms/new" component={NewRoom} />
           <Route path="/rooms/:id" component={Room} />
+
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
---
+++ src/pages/AdminRoom.tsx
@@ -0,0 +1,86 @@
+import { useHistory, useParams } from 'react-router-dom';
+
+import logoImg from '../assets/images/logo.svg';
+import deletImg from '../assets/images/delete.svg'
+
+import { Button } from '../components/Button';
+import { Question } from '../components/Question';
+import { RoomCode } from '../components/RoomCode';
+/* import { useAuth } from '../hooks/useAuth'; */
+import { useRoom } from '../hooks/useRoom';
+
+
+import '../styles/room.scss';
+import { database } from '../services/firebase';
+
+
+
+type RoomParams = {
+  id: string,
+};
+
+export function AdminRoom() {
+  /* const { user } = useAuth(); */
+  const history = useHistory()
+  const params = useParams<RoomParams>();
+  const roomId = params.id;
+
+  const { title, questions } = useRoom(roomId)
+
+  async function handleEndRoom(){
+   await  database.ref(`rooms/${roomId}`).update({
+     endedAt: new Date(),
+   })
+
+   history.push('/');
+  }
+
+
+  async function handleDeleteQuestion(questionId: string) {
+    if (window.confirm('Tem certeza que você deseja excluir esta pergunta?')){
+      await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
+    }
+  }
+
+
+  return (
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <img src={logoImg} alt="Letmeask" />
+          <div>
+          <RoomCode code={params.id} />
+          <Button isOutlined onClick={handleEndRoom}>Encerrar sala</Button>
+          </div>
+        </div>
+      </header>
+
+      <main>
+        <div className="room-title">
+          <h1>Sala {title}</h1>
+          {questions.length > 0 && <span>{questions.length} pergunta(s)</span>}
+        </div>
+        <div className='question-list'>
+        {questions.map(question =>{
+          return (
+            <Question
+              key={question.id}
+              content={question.content}
+              author={question.author}
+
+            >
+              <button
+              type='button'
+              onClick={() => handleDeleteQuestion(question.id)}
+              >
+
+                <img src={deletImg} alt='Remover pergunta' />
+              </button>
+              </Question>
+          );
+        })}
+        </div>
+      </main>
+    </div>
+  );
+}
